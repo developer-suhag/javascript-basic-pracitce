a4caaaafd7c63a01298e44da814a3ef8af3c3474
developer-suhag <developersuhag@gmail.com>
2021-08-05 14:46:41 +0600

    three number addition function

---
+++ problem-solving/three-number-adding-function.js
@@ -0,0 +1,7 @@
+function addition(number1, number2, number3) {
+    totalNumber = number1 + number2 + number3;
+    return totalNumber
+}
+
+let myAditionalNumbers = addition(32, 45, 65);
+console.log('The Addition result is: ', myAditionalNumbers);
